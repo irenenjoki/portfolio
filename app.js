40a0fd63ec196b55ac80b199fc7136aefde7b0f3
Irene Waweru <85219856+irenenjoki@users.noreply.github.com>
2023-06-30 15:16:56 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -33,4 +33,21 @@ navLinks.forEach( link => {
     navContent.classList.remove('show');
     document.body.style.overflow="initial";
   })
+
+    // JavaScript code to get the current date and year
+    var currentDate = new Date();
+    var year = currentDate.getFullYear();
+   
+
+    // Function to update the date and year
+    function updateDate() {
+      document.getElementById('current-year').textContent = "© IRENE. All right Reserved " + year;
+    }
+
+    // Update the date and year initially
+    updateDate();
+
+    // Update the date and year every second
+    setInterval(updateDate, 1000);
+
 })
